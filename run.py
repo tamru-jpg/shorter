edd35547daf095c5b64f9d3189747ac7fa3a79fd
Tamara <tamaraking1@yandex.ru>
2026-05-27 13:25:11 +0300

    1

--- run.py
+++ run.py
@@ -1,12 +1,8 @@
-from flask import render_template
-
 from app import create_app
+import os
 
 app = create_app()
 
-@app.route('/test-500')
-def test_500():
-    return render_template('errors/500.html')
 if __name__ == '__main__':
-    print("http://127.0.0.1:5000")
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
