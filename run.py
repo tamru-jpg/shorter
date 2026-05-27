f5d2bf8ed0ea9d5d0f65632568a9c06c01a0b55a
Tamara <tamaraking1@yandex.ru>
2026-05-27 13:54:34 +0300

    2

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
+print("Сервер запускается...")
 from app import create_app
 import os
 
 app = create_app()
+port = int(os.environ.get("PORT", 5000))
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    print(f"Запуск на порту {port}")
+    app.run(host="0.0.0.0", port=port)
